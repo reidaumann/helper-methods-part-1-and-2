d22a753d811ce8458385d4fff4c823b2fc8a3ee2
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-19 17:21:26 +0000

    refactoring movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,7 +24,7 @@ class MoviesController < ApplicationController
   def show
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where(id: the_id)
 
     @the_movie = matching_movies.first
 
@@ -47,7 +47,7 @@ class MoviesController < ApplicationController
   def edit
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where(id: the_id)
 
     @the_movie = matching_movies.first
 
@@ -56,7 +56,7 @@ class MoviesController < ApplicationController
 
   def update
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -71,7 +71,7 @@ class MoviesController < ApplicationController
 
   def destroy
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id).first
 
     the_movie.destroy
 
